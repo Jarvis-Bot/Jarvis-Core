35ac429c58a9929c440254bdf4ebbfc60a46d3a2
Victor Bersy <victorbersy+github@gmail.com>
2013-10-25 20:27:56 +0200

    add loader

---
+++ src/Loader.rb
@@ -0,0 +1,27 @@
+require 'yaml'
+require 'pp'
+class Loader
+  def self.listModules
+    modulesDirectories = Array.new
+
+    Dir['modules/*/'].each { |directory|
+      modulesDirectories.push(directory)
+    }
+
+    return modulesDirectories
+  end
+
+  def self.getInfos
+    modulesDirectories = self.listModules
+    modulesInfos = Array.new
+
+    modulesDirectories.each { |directory|
+      modulesInfos.push(YAML.load_file(directory + 'module.yml'))
+    }
+
+    return modulesInfos
+  end
+end
+
+pp Loader.listModules
+pp Loader.getInfos
