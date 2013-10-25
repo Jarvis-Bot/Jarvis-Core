18ad485ca528e59325dfd81c509f58dd72cdedc9
Victor Bersy <victor.bersy@gmail.com>
2013-10-25 19:16:19 +0200

    remove debug line

--- Clients.rb
+++ Clients.rb
@@ -13,7 +13,6 @@ class Clients
   end
 
   def self.getStream
-    puts @keys
     client = Twitter::Streaming::Client.new do |config|
       config.consumer_key        = @keys[:consumer_key]
       config.consumer_secret     = @keys[:consumer_secret]
