40f366969b0ad904142f9afd31cb4e32c77df057
Victor Bersy <victorbersy+github@gmail.com>
2013-10-26 20:53:53 +0200

    add Caller and Keys classes

--- src/jarvis.rb
+++ src/jarvis.rb
@@ -3,6 +3,9 @@ $: << File.dirname(__FILE__)
 require 'pp'
 require 'rainbow'
 require 'yaml'
+require 'twitter'
+
+require 'Jarvis/Config/Keys'
 
 require 'Jarvis/Display/Viewer'
 
@@ -12,6 +15,7 @@ require 'Jarvis/Plugins/Loader'
 
 require 'Jarvis/Twitter/Clients'
 require 'Jarvis/Twitter/Handler'
+require 'Jarvis/Twitter/Caller'
 require 'Jarvis/Twitter/Answer'
 
 require 'Jarvis/Starter'
