6e7c9de812dcb2b4de5e779ab8b72213964152db
Victor Bersy <victorbersy+github@gmail.com>
2013-10-27 00:25:12 +0200

    give Answer instance to Handler

--- src/Jarvis/Twitter/Handler.rb
+++ src/Jarvis/Twitter/Handler.rb
@@ -1,20 +1,21 @@
 module Jarvis
   class Handler
-    def initialize(tweet)
+    def initialize(tweet, answer_instance)
       loader = Loader.new
       @triggers_list           = loader.triggers_list
       @plugins_full_infos_list = loader.plugins_full_infos_list
+      @answer_instance = answer_instance
       receive_tweet(tweet)
     end
 
     def receive_tweet(tweet)
-      if self.match_triggers?(tweet.text)
+      if tweet.user.screen_name != "VBbot" && self.match_triggers?(tweet.text)
         send_tweet_to_plugin(@plugin_name, tweet, @trigger_detected)
       end
     end
 
     def send_tweet_to_plugin(plugin_name, tweet, trigger_detected)
-      caller = Caller.new(@plugins_full_infos_list[plugin_name], tweet, trigger_detected)
+      caller = Caller.new(@answer_instance, @plugins_full_infos_list[plugin_name], tweet, trigger_detected)
     end
 
     def match_triggers?(text)
