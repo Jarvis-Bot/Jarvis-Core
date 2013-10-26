d6b3831d6512bc05e13b72476e1d660dbe724397
Victor Bersy <victorbersy+github@gmail.com>
2013-10-26 17:38:53 +0200

    remove actions, add requires

--- src/jarvis.rb
+++ src/jarvis.rb
@@ -2,28 +2,18 @@ $: << File.dirname(__FILE__)
 
 require 'pp'
 require 'rainbow'
-require 'Jarvis/Handler.rb'
-require 'Jarvis/Clients.rb'
-require 'Jarvis/Answer.rb'
-require 'Jarvis/Loader.rb'
+require 'yaml'
 
-# -----------------------------
+require 'Jarvis/Display/Viewer'
 
-## Loadings plugins
-loader = Loader.new()
-pp loader.pluginsDirectories
-pp loader.pluginsTriggers
+require 'Jarvis/Plugins/Scanner'
+require 'Jarvis/Plugins/Lister'
+require 'Jarvis/Plugins/Loader'
 
-## Connect REST and Streaming clients
+require 'Jarvis/Twitter/Answer'
+require 'Jarvis/Twitter/Clients'
+require 'Jarvis/Twitter/Handler'
 
-clients = Hash.new()
-
-  # REST client, allow us to send reply
-  clients[:REST] = Clients.getREST
-  answer = Answer.new(clients[:REST])
-
-  # Stream client, watching for a new tweet
-  clients[:stream] = Clients.getStream
-  clients[:stream].user(:replies => 'all') { |tweet|
-    Handler.new(tweet, answer)
-  }
+module Jarvis
+  loader = Loader.new
+end
