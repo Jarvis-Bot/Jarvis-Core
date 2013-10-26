90a62521d6ac78dce40bd3d31abcc0ec12624f44
Victor Bersy <victorbersy+github@gmail.com>
2013-10-27 00:27:50 +0200

    bybye bitch, welcome "Hello World"

--- plugins/HelloWorld/HelloWorld.rb
+++ plugins/HelloWorld/HelloWorld.rb
@@ -1,8 +1,12 @@
 class HelloWorld
+  def initialize(answer_instance, tweet, trigger_detected)
+    @answer           = answer_instance
+    @tweet            = tweet
+    @trigger_detected = trigger_detected
+    self.send_back
+  end
 
-  def initialize(tweet, trigger_detected)
-    puts tweet.text 
-    puts trigger_detected
-    puts "HELLO BACK BIIIITTCCHH"
+  def send_back
+    @answer.reply(@tweet.user.screen_name, "Hello World !", @tweet.id)
   end
 end
