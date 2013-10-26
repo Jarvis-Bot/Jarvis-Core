d8e20d12d6e7927410652d7180906c5c1a031653
Victor Bersy <victorbersy+github@gmail.com>
2013-10-26 20:54:23 +0200

    add shared var

--- src/Jarvis/Plugins/Loader.rb
+++ src/Jarvis/Plugins/Loader.rb
@@ -1,10 +1,12 @@
 module Jarvis
   class Loader
-    attr_accessor :triggers_list
+    attr_accessor :triggers_list, :plugins_full_infos_list
     def initialize
       lister = Lister.new
-      @plugins_list = lister.list_plugins
-      @triggers_list = self.list_triggers
+      @plugins_list            = lister.list_plugins
+      @plugins_full_infos_list = self.index_full_infos(lister.list_full_infos_plugins)
+      @triggers_list           = self.list_triggers
+      self.require_plugins
     end
 
     def list_triggers
@@ -14,6 +16,21 @@ module Jarvis
         list_triggers = list_triggers.merge(to_merge)
       }
       return list_triggers
+    end
+
+    def index_full_infos(not_indexed_list)
+      indexed_full_infos = Hash.new
+      not_indexed_list.each { |plugin|
+        to_merge = {plugin["name"] => plugin}
+        indexed_full_infos = indexed_full_infos.merge(to_merge)
+      }
+      return indexed_full_infos
+    end
+
+    def require_plugins
+      @plugins_full_infos_list.each { |plugin_name, plugin_infos|
+        require "../plugins/" + plugin_infos["folder_name"] + "/" + plugin_infos["receiver"]
+      }
     end
   end
 end
