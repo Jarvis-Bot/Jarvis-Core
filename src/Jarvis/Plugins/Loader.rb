45880e37d7f76749cf5503a81c76485a98032708
Victor Bersy <victorbersy+github@gmail.com>
2013-10-26 17:59:23 +0200

    rename vars & methods

--- src/Jarvis/Plugins/Loader.rb
+++ src/Jarvis/Plugins/Loader.rb
@@ -1,14 +1,15 @@
 module Jarvis
   class Loader
+    attr_accessor :triggers_list
     def initialize
       lister = Lister.new
-      @list_plugins = lister.list_plugins
-      @list_triggers = self.list_triggers
+      @plugins_list = lister.list_plugins
+      @triggers_list = self.list_triggers
     end
 
     def list_triggers
       list_triggers = Hash.new
-      @list_plugins.each { |plugin|
+      @plugins_list.each { |plugin|
         to_merge = {plugin["name"] => plugin["triggers"]}
         list_triggers = list_triggers.merge(to_merge)
       }
