6582c6442898f25819dbde351e90e44d8d63b57e
Victor Bersy <victorbersy+github@gmail.com>
2013-10-26 20:54:55 +0200

    rename vars and indent

--- src/Jarvis/Plugins/Lister.rb
+++ src/Jarvis/Plugins/Lister.rb
@@ -1,22 +1,22 @@
 module Jarvis
   class Lister
-    attr_accessor :list_plugins
+    attr_accessor :list_plugins, :list_full_infos_plugins
     def initialize
       scanner = Scanner.new
-      @plugins_directories = scanner.plugins_directories
-      @plugins_infos       = self.get_infos
-      @list_plugins        = self.create_list
+      @plugins_directories     = scanner.plugins_directories
+      @list_full_infos_plugins = self.full_infos
+      @list_plugins            = self.create_list
     end
 
     def create_list
       list_plugins = Array.new
-      @plugins_infos.each{ |plugin|
+      @list_full_infos_plugins.each{ |plugin|
         list_plugins.push(plugin)
       }
       return list_plugins
     end
 
-    def get_infos
+    def full_infos
       plugins_infos = Array.new
 
       @plugins_directories.each { |directory|
