7f8a607049d494357daeecd28fee9574888df4b8
Victor Bersy <victorbersy+github@gmail.com>
2013-10-27 00:26:53 +0200

    add module & rename vars

--- src/Jarvis/Twitter/Answer.rb
+++ src/Jarvis/Twitter/Answer.rb
@@ -1,11 +1,15 @@
-class Answer
-  def initialize(rest_client)
-    @rest_client = rest_client
-  end
-  
+module Jarvis
+  class Answer
+    def initialize(rest_client)
+      @rest_client = rest_client
+    end
 
-  def byMention(screen_name, text, id)
-    @rest_client.update('@' + screen_name + ' ' + text, {:in_reply_to_status_id => id} )
+    def reply(screen_name, text, in_reply_to_id)
+      @rest_client.update('@' + screen_name + ' ' + text, {:in_reply_to_status_id => in_reply_to_id} )
+    end
+    
+    def update(text)
+      @rest_client.update(text)
+    end
   end
-  
 end
