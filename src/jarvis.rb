5f13dd2aa12a3298050ba26050155d9710f017ec
Victor Bersy <victorbersy+github@gmail.com>
2013-11-14 20:25:53 +0100

    add arguments and little help message

--- src/jarvis.rb
+++ src/jarvis.rb
@@ -1,25 +1,35 @@
 $: << File.dirname(__FILE__)
 
-require 'pp'
-require 'rainbow'
-require 'yaml'
-require 'twitter'
+JARVIS_HELP = <<-EOS
+Usage: ./jarvis [OPTION]
+  -c, --configure          configure Jarvis with Twitter API keys
+  -h, --help               display this message
+EOS
 
-require 'Jarvis/Config/Keys'
-
-require 'Jarvis/Display/Viewer'
-
-require 'Jarvis/Plugins/Scanner'
-require 'Jarvis/Plugins/Lister'
-require 'Jarvis/Plugins/Loader'
-
-require 'Jarvis/Twitter/Clients'
-require 'Jarvis/Twitter/Handler'
-require 'Jarvis/Twitter/Caller'
-require 'Jarvis/Twitter/Answer'
-
-require 'Jarvis/Starter'
-
-module Jarvis
-  Starter.new
+case ARGV.first
+when "-h", "--help", "help"
+  puts JARVIS_HELP
+when "-c" , "--configure", "configure"
+  puts ""
+when nil
+  require 'pp'
+  require 'rainbow'
+  require 'yaml'
+  require 'twitter'
+  require 'Jarvis/Config/Keys'
+  require 'Jarvis/Display/Viewer'
+  require 'Jarvis/Plugins/Scanner'
+  require 'Jarvis/Plugins/Lister'
+  require 'Jarvis/Plugins/Loader'
+  require 'Jarvis/Twitter/Clients'
+  require 'Jarvis/Twitter/Handler'
+  require 'Jarvis/Twitter/Caller'
+  require 'Jarvis/Twitter/Answer'
+  require 'Jarvis/Starter'
+  module Jarvis
+    Starter.new
+  end
+else
+  puts "Woops, there is something wrong. Take a look at the help :"
+  puts JARVIS_HELP
 end
