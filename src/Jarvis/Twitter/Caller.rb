a41baa7dd3a45b5325e5ddedc5a3ef29537e25db
Victor Bersy <victorbersy+github@gmail.com>
2013-11-14 16:22:18 +0100

    improve flexibility of the caller

--- src/Jarvis/Twitter/Caller.rb
+++ src/Jarvis/Twitter/Caller.rb
@@ -1,17 +1,18 @@
 module Jarvis
   class Caller
     def initialize(answer_instance, plugins_infos, tweet, trigger_detected)
-      @answer_instance     = answer_instance
       @plugins_infos       = plugins_infos
-      @tweet               = tweet 
-      @trigger_detected    = trigger_detected
-      @path_plugins_folder = "../plugins/"
 
+      @args = {
+        :answer_instance  => answer_instance,
+        :tweet            => tweet,
+        :trigger_detected => trigger_detected
+      }
       self.call_plugin
     end
 
     def call_plugin
-      Object::const_get(@plugins_infos["receiver_class_name"]).new(@answer_instance, @tweet, @trigger_detected)
+      Object::const_get(@plugins_infos["receiver_class_name"]).new(@args)
     end
   end
 end
