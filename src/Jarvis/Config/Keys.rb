1e8f2d647deea537b654b68c415150b87ba21bc0
Victor Bersy <victorbersy+github@gmail.com>
2013-11-14 16:42:42 +0100

    a bit cleaner

--- src/Jarvis/Config/Keys.rb
+++ src/Jarvis/Config/Keys.rb
@@ -7,9 +7,9 @@ module Jarvis
 
     def are_defined?
       are_defined = 
-        @keys["consumer_key"] != "YOUR_CONSUMER_KEY_HERE" &&
-        @keys["consumer_secret"] != "YOUR_CONSUMER_SECRET_HERE" &&
-        @keys["access_token"] != "YOUR_ACCESS_TOKEN_HERE" &&
+        @keys["consumer_key"]        != "YOUR_CONSUMER_KEY_HERE" &&
+        @keys["consumer_secret"]     != "YOUR_CONSUMER_SECRET_HERE" &&
+        @keys["access_token"]        != "YOUR_ACCESS_TOKEN_HERE" &&
         @keys["access_token_secret"] != "YOUR_ACCESS_TOKEN_SECRET_HERE"
       return are_defined
     end
--- src/Jarvis/Twitter/Caller.rb
+++ src/Jarvis/Twitter/Caller.rb
@@ -2,12 +2,12 @@ module Jarvis
   class Caller
     def initialize(answer_instance, plugins_infos, tweet, trigger_detected)
       @plugins_infos       = plugins_infos
-
       @args = {
         :answer_instance  => answer_instance,
         :tweet            => tweet,
         :trigger_detected => trigger_detected
       }
+      
       self.call_plugin
     end
 
