44276ca93ecbbd911e7b7fcfc710ad49fbe89b7f
Victor Bersy <victorbersy+github@gmail.com>
2014-01-03 23:30:07 +0100

    clean way to call clients

--- src/Jarvis/Twitter/Clients.rb
+++ src/Jarvis/Twitter/Clients.rb
@@ -1,29 +1,25 @@
 module Jarvis
   class Clients
-    attr_accessor :client_REST, :client_stream
-    def initialize
-      keys = Keys.new
-      @keys = keys.keys
-      @client_REST = start_client_REST
-      @client_stream = start_client_stream
+    def self.keys
+      keys = YAML.load_file('../config/keys.yml')
     end
 
-    def start_client_REST
+    def self.rest
       client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = @keys["consumer_key"]
-        config.consumer_secret     = @keys["consumer_secret"]
-        config.access_token        = @keys["access_token"]
-        config.access_token_secret = @keys["access_token_secret"]
+        config.consumer_key        = self.keys["consumer_key"]
+        config.consumer_secret     = self.keys["consumer_secret"]
+        config.access_token        = self.keys["access_token"]
+        config.access_token_secret = self.keys["access_token_secret"]
       end
       return client
     end
 
-    def start_client_stream
+    def self.stream
       client = Twitter::Streaming::Client.new do |config|
-        config.consumer_key        = @keys["consumer_key"]
-        config.consumer_secret     = @keys["consumer_secret"]
-        config.access_token        = @keys["access_token"]
-        config.access_token_secret = @keys["access_token_secret"]
+        config.consumer_key        = self.keys["consumer_key"]
+        config.consumer_secret     = self.keys["consumer_secret"]
+        config.access_token        = self.keys["access_token"]
+        config.access_token_secret = self.keys["access_token_secret"]
       end
       return client
     end
