53a4699ab6029fcabac8a93aec8bbb34babb3841
Victor Bersy <victorbersy+github@gmail.com>
2014-01-03 23:50:45 +0100

    clean require stuff

--- src/jarvis.rb
+++ src/jarvis.rb
@@ -12,16 +12,12 @@ module Jarvis
     Detector.new(ARGV)
   else
     require 'twitter'
-    require 'Jarvis/Config/Keys'
     require 'Jarvis/Display/Viewer'
-    require 'Jarvis/Plugins/Scanner'
-    require 'Jarvis/Plugins/Lister'
-    require 'Jarvis/Plugins/Loader'
+
     require 'Jarvis/Twitter/Clients'
-    require 'Jarvis/Twitter/Handler'
-    require 'Jarvis/Twitter/Caller'
-    require 'Jarvis/Twitter/Answer'
-    require 'Jarvis/Starter'
-    Starter.new
+    require 'Jarvis/Twitter/Dispatcher'
+
+    require 'Jarvis/Launcher'
+    Launcher::start
   end
 end
