bd070125480e6253913c2f174c866b81b01cabb6
Victor Bersy <victorbersy+github@gmail.com>
2014-01-03 23:50:55 +0100

    still really messy, working on it

--- src/Jarvis/Twitter/Handler.rb
+++ src/Jarvis/Twitter/Handler.rb
@@ -1,36 +1,37 @@
 module Jarvis
   class Handler
-    attr_accessor :is_matched
-    def initialize(tweet, answer_instance)
-      loader = Loader.new
-      @tweet                   = tweet
-      @triggers_list           = loader.triggers_list
-      @plugins_full_infos_list = loader.plugins_full_infos_list
-      @answer_instance         = answer_instance
-      @is_matched              = self.match_triggers?
-      receive_tweet
+    loader                   = Loader.new()
+    @triggers_list           = loader.triggers_list
+    @plugins_full_infos_list = loader.plugins_full_infos_list
+    def self.tweet(tweet)
+        if self.match_triggers?(tweet)
+            pp tweet.text
+        end
     end
 
-    def receive_tweet
-      if @tweet.user.screen_name != "VBbot" && @is_matched
-        send_tweet_to_plugin(@plugin_name, @trigger_detected)
-      end
+    def self.deleted_tweet(deleted_tweet)
     end
 
-    def send_tweet_to_plugin(plugin_name, trigger_detected)
-      caller = Caller.new(@answer_instance, @plugins_full_infos_list[plugin_name], @tweet, trigger_detected)
+    def self.direct_message(direct_message)
     end
 
-    def match_triggers?
-      @triggers_list.each { |plugin_name, plugin_triggers|
-        plugin_triggers.each { |trigger_word|
-          if @tweet.text.include?(trigger_word)
-            @trigger_detected = trigger_word
-            @plugin_name = plugin_name
-            return true
-          end
-        }
-      }
+    def self.event(event)
     end
-  end
+
+    def self.friend_list(friend_list)
+    end
+
+    private
+        def self.match_triggers?(tweet)
+          @triggers_list.each { |plugin_name, plugin_triggers|
+            plugin_triggers.each { |trigger_word|
+              if tweet.text.include?(trigger_word)
+                @trigger_detected = trigger_word
+                @plugin_name = plugin_name
+                return true
+              end
+            }
+          }
+        end
+    end
 end
