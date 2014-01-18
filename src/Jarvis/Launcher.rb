7d24d37246c748034aede38325284cdbb09e53a2
Victor Bersy <victor.bersy@gmail.com>
2014-01-17 18:37:35 -0800

    Jarvis will ask your twitter API keys if they doesn't exist

--- src/Jarvis/Launcher.rb
+++ src/Jarvis/Launcher.rb
@@ -1,8 +1,18 @@
 module Jarvis
   class Launcher
     def self.start
-      current_user = Clients::rest.verify_credentials(:include_entities => false, :skip_status => false)
-      Viewer::welcome_message(current_user)
+      unless File.exist?("../config/keys.yml")
+        puts "You need to configure Twitter API's keys in Jarvis. Go to http://dev.twitter.com/"
+        require 'Jarvis/CLI/Config'
+        Config::config_keys
+      end
+      self.start_jarvis
+    end
+
+    def self.start_jarvis
+      # Need SSL bugfix
+      # current_user = Clients::rest.verify_credentials(:include_entities => false, :skip_status => false)
+      # Viewer::welcome_message(current_user)
 
       Clients::stream.user(:with => 'user') do |message|
         Dispatcher::dispatch(message)
