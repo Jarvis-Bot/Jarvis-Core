8fbb890b1e236d5dd392a6a928df7c99aa1e4875
Victor Bersy <victor.bersy@gmail.com>
2014-01-23 23:09:40 -0800

    now it's a good example for future devs :)

--- plugins/HelloWorld/init.rb
+++ plugins/HelloWorld/init.rb
@@ -1,7 +1,12 @@
 class HelloWorld < Jarvis::Plugin
   def self.init(message)
-    tweet = "@#{message.user.screen_name}Hello back !"
-    options = :in_reply_to_status => message
-    Jarvis::Plugin::rest_client.update(tweet, options)
+    @tweet   = "@#{message.user.screen_name} Hello back !"
+    @options = {:in_reply_to_status => message}
+
+    self.send_tweet
+  end
+
+  def self.send_tweet
+    Jarvis::Plugin::rest_client.update(@tweet, @options)
   end
 end
