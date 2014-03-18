1525eeecb8117fa452d6b176fa850e5dc976206a
Victor Bersy <victorbersy+github@gmail.com>
2014-03-18 22:41:50 +0100

    add class name and start methods

--- src/Jarvis/boot/plugins.rb
+++ src/Jarvis/boot/plugins.rb
@@ -1,10 +1,14 @@
 module Jarvis
   module Boot
-    Utility::Logger.info('Scanning plugins directory...')
-    loaded_plugins = Plugins::Plugins.load
-    loaded_plugins.each do |plugin|
-      Utility::Logger.info("\"#{plugin['Plugin']['name']}\" by \"#{plugin['Author']['name']}\" loaded.")
+    class Plugins
+      def self.start
+        Jarvis::Utility::Logger.info('Scanning plugins directory...')
+        loaded_plugins = Jarvis::Plugins::Plugins.load
+        loaded_plugins.each do |plugin|
+          Jarvis::Utility::Logger.info("\"#{plugin['Plugin']['name']}\" by \"#{plugin['Author']['name']}\" loaded.")
+        end
+        Jarvis::Utility::Logger.info("Total : #{loaded_plugins.count} plugin(s) loaded.")
+      end
     end
-    Utility::Logger.info("Total : #{loaded_plugins.count} plugin(s) loaded.")
   end
 end
--- src/Jarvis/boot/sources.rb
+++ src/Jarvis/boot/sources.rb
@@ -1,11 +1,15 @@
 module Jarvis
   module Boot
-  	Utility::Logger.info('Starting Twitter streaming client...')
-  	Sources::Twytter::Streaming.client.user(with: 'user') do |message|
-  	  if message.is_a?(Twitter::Tweet)
-  	    Utility::Viewer::Message.tweet(message)
-  	    Messages::Factories::Twitter.new(message)
-  	  end
-  	end
+    class Sources
+      def self.start
+      	Jarvis::Utility::Logger.info('Starting Twitter streaming client...')
+      	Jarvis::Sources::Twytter::Streaming.client.user(with: 'user') do |message|
+      	  if message.is_a?(Twitter::Tweet)
+      	    Jarvis::Utility::Viewer::Message.tweet(message)
+      	    Jarvis::Messages::Factories::Twitter.new(message)
+      	  end
+        end
+    	end
+    end
   end
 end
