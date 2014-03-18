7d5fcda88bb5d762819c7d57951e6a2365f80f42
Victor Bersy <victorbersy+github@gmail.com>
2014-03-18 17:32:49 +0100

    add unique file for plugins loading

--- src/Jarvis/plugins/plugins_registered.rb
+++ src/Jarvis/plugins/plugins_registered.rb
@@ -2,14 +2,13 @@ module Jarvis
   module Plugins
     class Registered
       attr_reader :registered_plugins
-      def initialize
-        directories = Directories.new
-        @plugins_directories = directories.plugins_directories
+      def initialize(plugins_directories)
+        @plugins_directories = plugins_directories
         @registered_plugins  = []
-        load_plugins
+        registering_plugins
       end
 
-      def load_plugins
+      def registering_plugins
         @plugins_directories.each do |directory|
           plugin_specs_yaml = YAML.load_file(File.join(directory, 'plugin.yml'))
           plugin_specs_yaml['directory'] = directory
