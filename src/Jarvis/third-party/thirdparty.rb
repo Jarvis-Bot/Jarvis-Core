7318a61c35fcefdf1333c24b6e250905cb4f8379
Victor Bersy <victorbersy+github@gmail.com>
2014-04-19 20:55:52 +0200

    create unique colors, based on the name

--- src/Jarvis/third-party/thirdparty.rb
+++ src/Jarvis/third-party/thirdparty.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'digest'
 module Jarvis
   module ThirdParty
     class ThirdParty
@@ -16,12 +17,27 @@ module Jarvis
         registered = []
         Dir[@directory].each do |directory|
           if self.is_valid?(directory)
-            specs = YAML.load_file(@specs_file)
-            specs['directory'] = directory
-            registered.push specs
+            @specs = YAML.load_file(@specs_file)
+            @specs['directory'] = directory
+            self.check_color
+            registered.push @specs
           end
         end
         registered
+      end
+
+      def check_color
+        if @specs['specs']['color message'].nil?
+          self.generate_color
+        end
+      end
+
+      def generate_color
+        require 'pp'
+        name = @specs['specs']['name']
+        md5_name = Digest::MD5.hexdigest name
+        color = "##{md5_name[0..5].upcase}"
+        @specs['specs']['color message'] = color
       end
     end
   end
