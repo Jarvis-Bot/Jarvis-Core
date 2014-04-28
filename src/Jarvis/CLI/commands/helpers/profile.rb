adc69da078e5b49c56bdb531331798545b2493d2
Victor Bersy <victorbersy+github@gmail.com>
2014-04-29 00:19:13 +0200

    add profile helper

---
+++ src/Jarvis/CLI/commands/helpers/profile.rb
@@ -0,0 +1,22 @@
+require 'yaml'
+class Profile
+  def initialize(type)
+    @type = type
+    @file = "#{@type.to_s}.yml"
+    @path = File.expand_path(File.join('..', 'config', 'profiles', @file))
+  end
+
+  def display
+    puts @profile.to_s
+  end
+
+  def exists?
+    File.exists?(@path)
+  end
+
+  def load
+    @profile = YAML.load_file(@path)
+    rescue Errno::ENOENT => e
+      Jarvis::Utility::Logger.error(e, log: false)
+  end
+end
