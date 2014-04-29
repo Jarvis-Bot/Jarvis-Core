4b157fa5a547f8a77e5c3e5d8bcc8324e9fc797c
Victor Bersy <victorbersy+github@gmail.com>
2014-04-29 16:22:26 +0200

    questions methods are chainable

--- src/Jarvis/CLI/commands/helpers/questions.rb
+++ src/Jarvis/CLI/commands/helpers/questions.rb
@@ -8,19 +8,30 @@ class Questions
   end
 
   def ask(var, question, hint = nil)
+    @last_var = var.to_s
     print Rainbow("#{question} ").color(:magenta)
     print Rainbow("(#{hint})").color(:cyan) unless hint.nil?
     print Rainbow(': ').color(:magenta)
     @answers[var.to_s] = $stdin.gets.chomp.strip
+    self
   end
 
   def add(var, answer)
+    @last_var = var.to_s
     @answers[var.to_s] = answer
+    self
   end
 
-  def modify(var, &block)
-    answer = yield @answers[var.to_s]
-    @answers[var.to_s] = answer
+  def modify(var=nil, &block)
+    if var.nil?
+      answer = @answers[@last_var]
+      answer = yield answer
+      @answers[@last_var] = answer
+    else
+      answer = yield @answers[var.to_s]
+      @answers[var.to_s] = answer
+    end
+    self
   end
 
   def results
