fe21a7f68ec93a3d668d654cc57fb5d81fc45140
Victor Bersy <victorbersy+github@gmail.com>
2014-04-29 19:25:40 +0200

    add addon client generator

---
+++ src/Jarvis/CLI/commands/generators/addon_client.rb
@@ -0,0 +1,27 @@
+require 'yaml'
+require 'Jarvis/CLI/commands/helpers/addon_generator'
+require 'Jarvis/CLI/commands/helpers/questions'
+class AddonClient
+  def initialize
+    @client_generator = AddonGenerator.new(:client)
+    intro
+    fill_specs
+    generate_client
+  end
+
+  def intro
+    @client_generator.intro
+  end
+
+  def fill_specs
+    @client_generator.default_specs
+    @client_generator.ask_specific do
+      client = Questions.new(:client, 'Now, I have some specific questions about your client.')
+      client.ask(:service_name, "What's the name of the service used ?", "If it's a website, type the domain name without the extension, e.g: www.twitter.com = twitter")
+    end
+  end
+
+  def generate_client
+    @client_generator.generate_files
+  end
+end
