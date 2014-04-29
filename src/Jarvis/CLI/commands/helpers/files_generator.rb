2fb45400a500f62d3433a46b55a6224800b05637
Victor Bersy <victorbersy+github@gmail.com>
2014-04-29 19:47:24 +0200

    get informations and bind with ERB templates

--- src/Jarvis/CLI/commands/helpers/files_generator.rb
+++ src/Jarvis/CLI/commands/helpers/files_generator.rb
@@ -1,15 +1,17 @@
 require 'erb'
 require 'fileutils'
 class FilesGenerator
-  def initialize(template_path, file_name, path)
+  attr_accessor :infos
+  def initialize(template_path, file_name, path, infos)
     @template_path = template_path
     @file_name = file_name
     @path = path
+    @infos = infos
     @template = File.read(File.join(template_path, "#{file_name}.erb"))
   end
 
   def generate
-    @rendered = ERB.new(@template).result
+    @rendered = ERB.new(@template).result(binding)
     create_folder unless Dir.exist?(@path)
     File.open(File.join(@path, @file_name), 'w') do |f|
       f.write(@rendered)
