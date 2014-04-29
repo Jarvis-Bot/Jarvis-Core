b76b61bccd540f7005e7a127e0c7f5c214dbe4d2
Victor Bersy <victorbersy+github@gmail.com>
2014-04-29 18:43:29 +0200

    rename variables

--- src/Jarvis/CLI/commands/helpers/questions.rb
+++ src/Jarvis/CLI/commands/helpers/questions.rb
@@ -7,31 +7,35 @@ class Questions
     @theme = theme.to_s
   end
 
-  def ask(var, question, hint = nil)
-    @last_var = var.to_s
+  def ask(var_name, question, hint = nil)
+    @last_entry = var_name.to_s
     print Rainbow("#{question} ").color(:magenta)
     print Rainbow("(#{hint})").color(:cyan) unless hint.nil?
     print Rainbow(': ').color(:magenta)
-    @answers[var.to_s] = $stdin.gets.chomp.strip
+    @answers[var_name.to_s] = $stdin.gets.chomp.strip
     self
   end
 
-  def add(var, answer)
-    @last_var = var.to_s
-    @answers[var.to_s] = answer
+  def add(var_name, answer)
+    @last_entry = var_name.to_s
+    @answers[var_name.to_s] = answer
     self
   end
 
-  def modify(var=nil, &block)
-    if var.nil?
-      answer = @answers[@last_var]
+  def modify(var_name=nil, &block)
+    if var_name.nil?
+      answer = @answers[@last_entry]
       answer = yield answer
-      @answers[@last_var] = answer
+      @answers[@last_entry] = answer
     else
-      answer = yield @answers[var.to_s]
-      @answers[var.to_s] = answer
+      answer = yield @answers[var_name.to_s]
+      @answers[var_name.to_s] = answer
     end
     self
+  end
+
+  def retrieve(var_name)
+    @answers[var_name.to_s]
   end
 
   def results
