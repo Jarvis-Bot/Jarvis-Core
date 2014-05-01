b4dc0adc267d61a6f94fdcc932f8cebcc7d98d60
Victor Bersy <victorbersy+github@gmail.com>
2014-05-01 15:36:51 +0200

    fix typo

--- src/Jarvis/CLI/commands/generators/addon_receiver.rb
+++ src/Jarvis/CLI/commands/generators/addon_receiver.rb
@@ -3,29 +3,29 @@ require 'Jarvis/CLI/commands/helpers/addon_generator'
 require 'Jarvis/CLI/commands/helpers/questions'
 module Jarvis
   module CLI
-    class AddonReiceiver
+    class AddonReceiver
       def initialize
-        @reiceiver_generator = AddonGenerator.new(:reiceiver)
+        @receiver_generator = AddonGenerator.new(:receiver)
         intro
         fill_specs
-        generate_reiceiver
+        generate_receiver
       end
 
       def intro
-        @reiceiver_generator.intro
+        @receiver_generator.intro
       end
 
       def fill_specs
-        @reiceiver_generator.default_specs
-        @reiceiver_generator.ask_specific do
-          reiceiver = Questions.new(:reiceiver, 'Now, I have some specific questions about your reiceiver.')
-          reiceiver.ask(:service_name, "What's the name of the service used ?", "If it's a website, type the domain name without the extension, e.g: www.twitter.com = twitter")
-          reiceiver.ask(:color_message, "Your message need some color. What's yours?", 'If the service used have some designer guideline, enter the primary color')
+        @receiver_generator.default_specs
+        @receiver_generator.ask_specific do
+          receiver = Questions.new(:receiver, 'Now, I have some specific questions about your receiver.')
+          receiver.ask(:service_name, "What's the name of the service used ?", "If it's a website, type the domain name without the extension, e.g: www.twitter.com = twitter")
+          receiver.ask(:color_message, "Your message need some color. What's yours?", 'If the service used have some designer guideline, enter the primary color')
         end
       end
 
-      def generate_reiceiver
-        @reiceiver_generator.generate_files
+      def generate_receiver
+        @receiver_generator.generate_files
       end
     end
   end
