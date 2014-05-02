b3f97e73a561e98b758217e6a628a5825246cea8
Victor Bersy <victorbersy+github@gmail.com>
2014-05-02 21:54:41 +0200

    add general class to register addons

---
+++ src/Jarvis/addons/addons.rb
@@ -0,0 +1,55 @@
+require 'yaml'
+module Jarvis
+	module Addons
+    class Addons
+    attr_reader :type, :path, :validated
+      def initialize(type)
+        @type = type
+        @path = File.join(JARVIS[:root], 'addons', @type.to_s)
+        @validated = validated
+        display
+      end
+
+      def validated
+        @validated = []
+        Dir.glob(File.join(@path, '*')) do |addon_dir|
+          @addon_dir = addon_dir
+          if valid?
+            addon = {}
+            addon[:path] = @addon_dir
+            addon[:informations] = full_specs
+            addon[:informations]['directory'] = @addon_dir
+            @validated.push addon
+          end
+        end
+        @validated
+      end
+
+      def valid?
+        File.exist?(File.join(@addon_dir, 'specs.yml')) &&
+        File.exist?(File.join(@addon_dir, 'init.rb'))
+      end
+
+      def full_specs
+        YAML.load_file(File.join(@addon_dir, 'specs.yml'))
+      end
+
+      def count
+        @validated.count
+      end
+
+      def display
+        Jarvis::Utility::Logger.info("#{count} #{type.capitalize} installed.")
+        @validated.each_with_index  do |addon, index|
+
+          tree_char = (index + 1 == count) ? '└' : '├'
+
+          author = addon[:informations]['author']['name']
+          name = addon[:informations]['specs']['name']
+
+          Utility::Logger.info("#{tree_char}───[#{author}] #{name}.")
+        end
+      end
+    end
+  end
+end
