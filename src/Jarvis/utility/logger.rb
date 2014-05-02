162f80610f5b8985ea3c3b34affcd48c52289f41
Victor Bersy <victorbersy+github@gmail.com>
2014-05-02 19:01:43 +0200

    add date to each log

--- src/Jarvis/utility/logger.rb
+++ src/Jarvis/utility/logger.rb
@@ -6,7 +6,7 @@ module Jarvis
       # A trick to call this class like "Logger.info" instead of "Logger.new.info"
       class << self
         def add_log_file(log)
-          log_file_name = "#{@@time.strftime('%Y-%m-%d %H.%M.%S %z')}.log"
+          log_file_name = "#{@@time.strftime('%Y-%m-%d %H.%M.%S')}.log"
           log_folder = File.join('..', 'logs')
           Dir.mkdir(log_folder, 0755) unless Dir.exist?(log_folder)
           File.open(File.join(log_folder, log_file_name), 'a') do |file|
@@ -16,7 +16,7 @@ module Jarvis
 
         %w(debug info warning error).each do |type_log|
           define_method type_log.to_sym do |log, options = { block: false, log: true }|
-            add_log_file("[#{type_log.upcase}] #{log}") if options[:log]
+            add_log_file("#{@@time.strftime('%d/%m/%Y %H.%M.%S')} [#{type_log.upcase}] #{log}") if options[:log]
             Viewer::Log.log_factory(type_log, log, options)
           end
         end
