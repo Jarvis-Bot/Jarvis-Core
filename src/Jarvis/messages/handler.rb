d41318667619a2f4246f8958eaad3a0cd8fb717f
Victor Bersy <victorbersy+github@gmail.com>
2014-05-04 18:17:38 +0200

    update handler for 2.0

--- src/Jarvis/messages/handler.rb
+++ src/Jarvis/messages/handler.rb
@@ -6,21 +6,21 @@ module Jarvis
         @from = message.from
         @message = message.text
         @message_object = message.message_object
-        @sorted_receivers = Boot::Session.sorted_registered_receivers
         dispatcher
       end
 
       def dispatcher
-        call_receiver(:all) unless @sorted_receivers[:all].nil?
-        call_receiver(@from) unless @sorted_receivers[@from].nil?
+        call_receivers(Jarvis::API::Addons.call_receivers_for(:all))
+        call_receivers(Jarvis::API::Addons.call_receivers_for(@from))
       end
 
-      def call_receiver(from)
-        @sorted_receivers[from].each do |receiver|
-          args = { timestamp: @timestamp, message: @message, from: @from, to: receiver[:name], object: @message_object }
+      def call_receivers(receivers)
+        receivers.each do |receiver|
+          receiver = receiver.values[0]
+          args = { timestamp: @timestamp, message: @message, from: @from, to: receiver['specs']['name'], object: @message_object }
           Utility::Viewer::Message.new(args)
-          require File.join("#{receiver[:directory]}", 'init.rb')
-          Object.const_get("#{receiver[:class_name]}").new(args)
+          require File.join("#{receiver['directory']}", 'init.rb')
+          Object.const_get("#{receiver['specs']['class_name']}").new(args)
         end
       end
     end
