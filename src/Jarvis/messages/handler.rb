27215cdada9af7feb61f245ecedd2c0b00427739
Victor Bersy <victorbersy+github@gmail.com>
2014-05-05 17:45:32 +0200

    check if receiver is found

--- src/Jarvis/messages/handler.rb
+++ src/Jarvis/messages/handler.rb
@@ -10,14 +10,18 @@ module Jarvis
       end
 
       def dispatcher
-        call_receivers(Jarvis::API::Addons.call_receivers_for(:all))
-        call_receivers(Jarvis::API::Addons.call_receivers_for(@from))
+        all = call_receivers(Jarvis::API::Addons.call_receivers_for(:all))
+        from = call_receivers(Jarvis::API::Addons.call_receivers_for(@from))
+        if (all.empty? && from.empty?)
+          args = { timestamp: @timestamp, message: @message, from: @from, found: false }
+          Utility::Viewer::Message.new(args)
+        end
       end
 
       def call_receivers(receivers)
         receivers.each do |receiver|
           receiver = receiver.values[0]
-          args = { timestamp: @timestamp, message: @message, from: @from, to: receiver['specs']['name'], object: @message_object }
+          args = { timestamp: @timestamp, message: @message, from: @from, to: receiver['specs']['name'], object: @message_object, found: true}
           Utility::Viewer::Message.new(args)
           require File.join("#{receiver['directory']}", 'init.rb')
           Object.const_get("#{receiver['specs']['class_name']}").new(args)
