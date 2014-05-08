67a0b603cfedc91c0c3b3a45ec734e1bb87d9fbe
Victor Bersy <victorbersy+github@gmail.com>
2014-05-08 16:38:39 +0200

    you can specify a color as an option

--- src/Jarvis/CLI/stdio.rb
+++ src/Jarvis/CLI/stdio.rb
@@ -68,15 +68,15 @@ module Jarvis
         `stty #{old_state}`
       end
 
-      def yes?(question, options = { color: true })
-        print options[:color] ? Rainbow("#{question} [Y/n]").color(:green) : "#{question} [Y/n]"
+      def yes?(question, options = { color: :green, colored: true })
+        print (options[:colored] || options[:colored].nil?)  ? Rainbow("#{question} [Y/n]").color(options[:color].to_sym) : "#{question} [Y/n]"
         answer = $stdin.gets.chomp.strip
         yes_cases = ['yes', 'YES', 'y', 'Y', '']
         yes_cases.include? answer
       end
 
-      def no?(question, options = { color: true })
-        print options[:color] ? Rainbow("#{question} [N/y]").color(:red) : "#{question} [N/y]"
+      def no?(question, options = { color: :red, colored: true })
+        print (options[:colored] || options[:colored].nil?)  ? Rainbow("#{question} [N/y]").color(options[:color].to_sym) : "#{question} [N/y]"
         answer = $stdin.gets.chomp.strip
         no_cases = ['no', 'NO', 'n', 'N', '']
         no_cases.include? answer
